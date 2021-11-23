d35fbc6cdef552021dfa8e72955fb22721cabe2f
jayce1116 <jaycesub@gmail.com>
2021-11-23 22:26:21 +0900

    Init Commit

---
+++ AppStoreClone/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AppStoreClone
+//
+//  Created by 신정섭 on 2021/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
